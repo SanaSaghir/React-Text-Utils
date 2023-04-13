b2a0ea17d20766f73e2806b8e80978cf306bcd75
Sana <sanasaghir979@gmail.com>
2023-04-13 10:17:03 +0500

    Updates

---
+++ precache-manifest.30668267029028ef3398367d2e5a0e0b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cd50fbf762604579b285",
+    "url": "/React-Text-Utils/static/css/main.03df85b0.chunk.css"
+  },
+  {
+    "revision": "cd50fbf762604579b285",
+    "url": "/React-Text-Utils/static/js/main.cd50fbf7.chunk.js"
+  },
+  {
+    "revision": "444f0bc38a785630c6eb",
+    "url": "/React-Text-Utils/static/js/1.444f0bc3.chunk.js"
+  },
+  {
+    "revision": "3c016175441de394fef9",
+    "url": "/React-Text-Utils/static/js/2.3c016175.chunk.js"
+  },
+  {
+    "revision": "1de3fd5eded10a1ba082",
+    "url": "/React-Text-Utils/static/js/runtime~main.1de3fd5e.js"
+  },
+  {
+    "revision": "8de323d7638445fd117f40fe81fd1356",
+    "url": "/React-Text-Utils/index.html"
+  }
+];
